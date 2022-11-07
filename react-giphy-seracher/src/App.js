49f50c283f5fac87d4e1b127146dbd208df3dc3e
mobrewer <morgiearan@gmail.com>
2022-11-06 21:11:48 -0700

    Added ability to search for minion giphs and only display 25

--- react-giphy-seracher/src/App.js
+++ react-giphy-seracher/src/App.js
@@ -1,52 +1,47 @@
 import React from 'react';
 import SearchForm from './components/SearchForm';
 import SearchResults from './components/SearchResults';
-
-const images = [
-  {
-    id: 1,
-    url: 'https://media.giphy.com/media/3DsNP07nApt1eEyjvM/giphy.gif'
-  },
-  {
-    id: 2,
-    url: 'https://media1.giphy.com/media/26BRv0ThflsHCqDrG/giphy.gif'
-  },
-  {
-    id: 3,
-    url:
-      'https://www.thisiscolossal.com/wp-content/uploads/2018/04/agif2opt.gif'
-  },
-  {
-    id: 4,
-    url: 'https://media.giphy.com/media/3oEdva9BUHPIs2SkGk/giphy.gif'
-  },
-  {
-    id: 5,
-    url: 'https://media.giphy.com/media/3o6ozoD1ByqYv7ARIk/giphy.gif'
-  },
-  {
-    id: 6,
-    url: 'https://buffer.com/library/wp-content/uploads/2016/06/giphy.gif'
-  },
-  {
-    id: 7,
-    url: 'https://media4.giphy.com/avatars/default5.gif'
-  },
-  {
-    id: 8,
-    url: 'https://media2.giphy.com/media/3oEduPff5ErjNmlbwY/source.gif'
-  }
-];
+import { useState } from 'react';
+import { useEffect} from 'react';
 
 
 function App() {
-  return (
+  const searchOptions = {
+    key: 'ZldMm0pgatlNVT0xep6kywlinLn2dBoy',
+    limit: 25,
+    rating: 'G',
+    api: 'https://api.giphy.com/v1/gifs',
+    endpoint: '/search'
+  };
+  console.log(searchOptions.api);
+
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    getImages();
+  }, []);
+
+   return (
     <div>
       <h1>Giphy Searcher</h1>
       <SearchForm />
       <SearchResults images={images}/>
     </div>
   );
+
+  function getImages() {
+    const searchString = 'minions';
+    /* Build a URL from the searchOptions object */
+    const url = `${searchOptions.api}${searchOptions.endpoint}?api_key=${searchOptions.key}&q=${searchString} &limit=${searchOptions.limit}&offset=${searchOptions.offset}&rating=${searchOptions.rating}&lang=en`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        setImages(response.data);
+      })
+      .catch(console.error);
+  }
+ 
+
 }
 
 export default App;
