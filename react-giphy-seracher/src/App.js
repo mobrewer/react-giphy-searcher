b7406a8cfcc1d7ba7d8535cb71dcec94f0a116e8
mobrewer <morgiearan@gmail.com>
2022-11-06 22:46:32 -0700

    Added final styling and added icons

--- react-giphy-seracher/src/App.js
+++ react-giphy-seracher/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchForm from './components/SearchForm';
 import SearchResults from './components/SearchResults';
-import { useState } from 'react';
-import { useEffect} from 'react';
+import SearchHeader from './components/SearchHeader';
+
 
 
 function App() {
+
+  const [images, setImages] = useState([]);
+  const [searchString, setSearchString] = useState('minions');
+  const [lastSearch, setLastSearch] = useState('');
+
+  function handleChange(event) {
+    setSearchString(event.target.value);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    getImages();
+  }
+  
+
   const searchOptions = {
     key: 'ZldMm0pgatlNVT0xep6kywlinLn2dBoy',
     limit: 25,
@@ -13,35 +28,41 @@ function App() {
     api: 'https://api.giphy.com/v1/gifs',
     endpoint: '/search'
   };
-  console.log(searchOptions.api);
 
-  const [images, setImages] = useState([]);
   useEffect(() => {
-    getImages();
+    // Pass the searchString to getImages
+    getImages(searchString);
   }, []);
 
-   return (
-    <div>
-      <h1>Giphy Searcher</h1>
-      <SearchForm />
-      <SearchResults images={images}/>
-    </div>
-  );
-
-  function getImages() {
-    const searchString = 'minions';
-    /* Build a URL from the searchOptions object */
-    const url = `${searchOptions.api}${searchOptions.endpoint}?api_key=${searchOptions.key}&q=${searchString} &limit=${searchOptions.limit}&offset=${searchOptions.offset}&rating=${searchOptions.rating}&lang=en`;
+  function getImages(searchString) {
+    const url = `${searchOptions.api}${searchOptions.endpoint}?api_key=${searchOptions.key}&q=${searchString}&limit=${searchOptions.limit}&offset=${searchOptions.offset}&rating=${searchOptions.rating}&lang=en`;
 
     fetch(url)
       .then(response => response.json())
       .then(response => {
         setImages(response.data);
+        // Set the lastSearch to the searchString value
+        setLastSearch(searchString);
+        // Set the searchString in state to an empty string
+        setSearchString('');
       })
       .catch(console.error);
   }
- 
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    // Don't forget to pass the searchString to getImages
+    getImages(searchString);
+  }
+
+
+  return (
+    <div>
+      <SearchForm handleChange={handleChange} handleSubmit={handleSubmit} searchString={searchString}/>
+      <SearchHeader lastSearch={lastSearch} />
+      <SearchResults images={images} />
+    </div>
+  );
 }
 
 export default App;
--- react-giphy-seracher/src/components/SearchForm.js
+++ react-giphy-seracher/src/components/SearchForm.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { SearchIcon } from '../shared/AppIcons';
 
-function SearchForm() {
+
+  // Destructure props object
+  function SearchForm({ handleSubmit, handleChange, searchString }) {
     return (
-        <form className="form-horizontal">
-            <input placeholder="Search" type="text" name="searchString" required />
-            <button type="submit">Search</button>
-        </form>
-    )
-}
+      <form onSubmit={handleSubmit} className="form-horizontal">
+        <input
+          placeholder="Search"
+          type="text"
+          name="searchString"
+          required
+          onChange={handleChange}
+          value={searchString}
+        />
+        <button type="submit">
+            <SearchIcon height="2rem" width="2rem" />
+        </button>
+      </form>
+    );
+  }
+  
 export default SearchForm
